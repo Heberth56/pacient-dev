c662f6b4382575c7a4513224c7a03268faec5930
dedstinguerdsds <dedstinguerdsds@gmail.com>
2024-11-04 00:11:59 -0400

    navigate from button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Hom ffdsfe</div>;
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/example");
+  };
+  return (
+    <div>
+      Home
+      <button onClick={handleNavigate}>Navegar Home</button>
+    </div>
+  );
 };
 
 export default Home;
